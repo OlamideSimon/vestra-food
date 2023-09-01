874265a7966cc60a0d3c2017178506644c0250cb
Olamide <simonadepetoye@gmail.com>
2023-09-01 18:13:44 +0100

    Testimonial card fixes

--- components/TestimonialCard.tsx
+++ components/TestimonialCard.tsx
@@ -11,7 +11,7 @@ const TestimonialCard = ({ comment, job, name }: CardType) => {
     <div className="max-w-[450px] shadow-2xl py-10 px-5 border border-slate-500 rounded-xl mx-auto first:col-span-12 col-span-12 lg:col-span-6 tracking-wider">
       <p className="text-4xl">{name}</p>
       <p className="text-xl">{job}</p>
-      <p className="mt-5 text-xl">"{comment}"</p>
+      <p className="mt-5 text-xl">&quot;{comment}&quot;</p>
     </div>
   )
 }
